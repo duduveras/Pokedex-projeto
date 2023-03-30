eda7e37014bf45efeaa46cb1a1fb3b34f04f7972
Eduardo <dudu@veras.com>
2023-03-30 17:44:06 -0300

    pokedex parte final

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react"
+import { PokerChip, MagnifyingGlass, Hash } from "phosphor-react";
+import { Card } from "./componets/Card";
+import { Loading } from "./componets/loading";
+
+  interface IPokemon {
+    name: string;
+    url: string;
+  } 
+
+function App() {
+   const [pokemon, setPokemons] = useState<IPokemon[]>([]);
+   const [loading, setLoading] = useState(true);
+   const [input, setInput] = useState("");
+
+    async function fetchData(){
+      const data = await
+      fetch('https://pokeapi.co/api/v2/pokemon?limit=120')
+      .then((Response) => Response.json()) 
+      .finally(() => setLoading(false))
+      
+      setPokemons(data.results);
+   }
+    useEffect(() => {
+      fetchData();
+    }, [])
+    if (loading) return <Loading/>;
+    return (
+    <div className="flex flex-col mb-12">
+      <header className="flex flex-col bg-red-500 w-full items-start px-6 py-4 drop-shadow-lg">
+      <div className="flex items-center gap-4 text-white font-bold py-4">
+        <PokerChip
+        size={44}
+        />
+
+      <h1 className="text-3xl">pokedex</h1>
+        </div>
+
+        <div className="flex w-full gap-12 items-center justify-between">
+        <div className="flex flex-1 border bg-white rounded-full items-center">
+        <span className="flex pl-4">
+          <MagnifyingGlass size={25} weight="bold" className="text-red-700"/>
+        </span>
+
+        <input
+        type="text"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+        placeholder="Digite o nome de um pokemon"
+        className="w-full py-3 px-6 rounded-full text-red-700 placeholder:text-gray-600 focus:outline-none"
+        />
+          </div>
+          <Hash size={46} weight="bold" className="bg-white text-red-600 rounded-full p-3"/>
+          </div>
+        </header>
+        <main className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 pt-12 gap-3 mx-auto">
+        {pokemon.filter((filteredItem) => filteredItem.name.includes(input)).map((item, index) => (
+          <Card
+          key={item.name}
+          name={item.name}
+          position={item.url}
+          />
+        ))}
+        </main>
+
+      
+    </div>
+    )
+  }
+
+export default App
